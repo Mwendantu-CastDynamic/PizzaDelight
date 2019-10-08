c453a0d321d650b2721bccff0f8e55fdf3450d92
Brian Muchunku <brauxer@gmail.com>
2019-10-08 05:14:58 +0300

    Initialize JavaScript Constructor and Functions for our Pizza Website pre-object declaration and designation

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+function Pizza(name, basicPrice, size, sizingCost, crust, crustCost, toppings, toppingsCost, totalCost)
+{
+  this.name = name;
+  this.basicPrice = basicPrice;
+  this.size = size;
+  this.sizingCost = sizingCost;
+  this.crust = crust;
+  this.crustCost = crustCost;
+  this.toppings= toppings;
+  this.toppingsCost = toppingsCost;
+  this.totalCost = totalCost;
+};
+  
+function sizingCost(small, medium, large){
+
+}
+function crustCost(size, crust){
+
+}
+function toppingsCost(size,toppings){
+
+}
+
+function totalCost(basePrice, sizingCost, crustCost, toppingsCost){
+  
+}
+
+let anchovySuprise= new Pizza
